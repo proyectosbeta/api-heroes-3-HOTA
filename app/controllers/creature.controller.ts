7795b815a303f453de2ebfd6404b2f00013da5d8
josego <josego85@gmail.com>
2021-04-18 10:38:27 -0400

    Refactoring code.

---
+++ app/controllers/creature.controller.ts
@@ -0,0 +1,54 @@
+import { Creature } from "../models/Creature.ts";
+import { castleCreatures } from "../data/castleCreatures.ts";
+import { rampartCreatures } from "../data/rampartCreatures.ts";
+
+let creatures: any;
+
+const factories: any = { 
+    'castle': castleCreatures,
+    'rampart': rampartCreatures,
+};
+
+const setFactory = (factory: string) => {
+    creatures = new Creature(getDataCreatures(factory));
+};
+
+const getDataCreatures = (factory: string) => {
+    var allCreatures = castleCreatures.concat(rampartCreatures);
+
+    return factories[factory] || allCreatures;
+};
+
+// Return all creatures.
+const getCreatures = ({
+    params,
+    response 
+}: {
+    params: { factory: string };
+    response: any
+}) => {
+  setFactory(params.factory);
+
+  response.body = creatures.getCreatures();
+};
+
+// Return creature by id.
+const getCreature = ({
+    params,
+    response,
+}: {
+    params: { id: string };
+    response: any;
+}) => {
+  const creature = creatures.getCreature(params.id);
+
+  if (creature) {
+      response.status = 200;
+      response.body = creature;
+  } else {
+      response.status = 404;
+      response.body = { message: "404 Not found" };
+  }
+};
+
+export { getCreatures, getCreature };
--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,21 +1,14 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
 import {
-  getCreatures as getcastleCreatures,
-  getCreature as getcastleCreature,
-} from '../controllers/castle/creature.controller.ts';
-import {
-  getCreatures as getRampartCreatures,
-  getCreature as getRampartCreature,
-} from '../controllers/rampart/creature.controller.ts';
+  getCreatures,
+  getCreature,
+} from '../controllers/creature.controller.ts';
 
 const router = new Router();
 
 router
-  .get('/castle/creature', getcastleCreatures)
-  .get('/castle/creature/:id', getcastleCreature);
-
-router
-  .get('/rampart/creature', getRampartCreatures)
-  .get('/rampart/creature/:id', getRampartCreature);
+  .get('/creature/', getCreatures)
+  .get('/creature/:factory', getCreatures)
+  // .get('/creature/:factory/:id', getCreature);
 
 export default router;
