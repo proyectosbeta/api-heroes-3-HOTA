230c7bb41b9080014bbfea3e28c5a8f197b47412
josego <josego85@gmail.com>
2021-06-27 09:40:29 -0400

    Add images in air spells

--- app/data/spells/air.ts
+++ app/data/spells/air.ts
@@ -1,4 +1,5 @@
 import { Spell } from "../../types/spell.ts";
+import { APP_DOMAIN } from '../../config/index.ts';
 
 export const airSpell: Array<Spell> = [
   {
@@ -7,14 +8,14 @@ export const airSpell: Array<Spell> = [
     level: 1,
     effect: "Target (or all) allied troop's speed is " +
       "increased by 3 (or 5) hexes per turn",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Haste.png`,
   },
   {
     id: "35",
     name: "View Air",
     level: 1,
     effect: "Reveals location of all unclaimed artifacts",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/View_Air.png`,
   },
   {
     id: "36",
@@ -22,7 +23,7 @@ export const airSpell: Array<Spell> = [
     level: 2,
     effect: "Modifies the army composition scouting report " +
       "visible to enemies",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Disguise.png`,
   },
   {
     id: "37",
@@ -30,21 +31,21 @@ export const airSpell: Array<Spell> = [
     level: 2,
     effect: "Reduces defense rating of a single enemy. " +
       "May be used repeatedly on the same target",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Disrupting_Ray.png`,
   },
   {
     id: "38",
     name: "Fortune",
     level: 2,
     effect: "Increases luck. Basic: Single ally. Expert: All allies",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Fortune.png`,
   },
   {
     id: "39",
     name: "Lightning Bolt",
     level: 2,
     effect: "Inflicts magic damage on a single enemy",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Lightning_Bolt.png`,
   },
   {
     id: "40",
@@ -52,7 +53,7 @@ export const airSpell: Array<Spell> = [
     level: 2,
     effect: " 	Increases ranged attack rating. Basic: Single " +
       "ally. Expert: All allies",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Precision.png`,
   },
   {
     id: "41",
@@ -61,28 +62,28 @@ export const airSpell: Array<Spell> = [
     effect: "Reduces damage taken from air spells. Basic: " +
       "Single ally. Expert: All allies. Effect increased " +
       "from 30%-30%-50%-50% to 50%-50%-75%-75%. Horn of the Abyss",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Protection_from_Air.png`,
   },
   {
     id: "42",
     name: "Air Shield",
     level: 3,
-    effect: " 	Reduces damage taken from ranged attacks",
-    imageUrl: "",
+    effect: "Reduces damage taken from ranged attacks",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Air_Shield.png`,
   },
   {
     id: "43",
     name: "Destroy Undead",
     level: 3,
     effect: "Inflicts magic damage on all undead creatures",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Destroy_Undead.png`,
   },
   {
     id: "44",
     name: "Hypnotize",
     level: 3,
     effect: "Allows control of actions taken by enemy creature",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Hypnotize.png`,
   },
   {
     id: "45",
@@ -90,7 +91,7 @@ export const airSpell: Array<Spell> = [
     level: 4,
     effect: "Inflicts magic damage on initial creature and " +
       "reduced damage on nearby creatures",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Chain_Lightning.png`,
   },
   {
     id: "46",
@@ -98,14 +99,14 @@ export const airSpell: Array<Spell> = [
     level: 4,
     effect: " 	Allows extra retaliations per round. Basic: " +
       "Single ally. Expert: All allies",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Counterstrike.png`,
   },
   {
     id: "47",
     name: "Dimension Door",
     level: 5,
     effect: "Teleports hero to visible location",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Dimension_Door.png`,
   },
   {
     id: "48",
@@ -114,7 +115,7 @@ export const airSpell: Array<Spell> = [
     effect: "Allows movement to visible land over " +
       "all map obstacles. Cannot fly through cave " +
       "walls in the underground",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Fly.png`,
   },
   {
     id: "49",
@@ -123,14 +124,14 @@ export const airSpell: Array<Spell> = [
     effect: "Reflects hostile spells to a random enemy. Basic: " +
       "20% chance of occurrence. Advanced: 30% chance. Expert: " +
       "40% chance",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Magic_Mirror.png`,
   },
   {
     id: "50",
     name: "Summon Air Elemental",
     level: 5,
     effect: "Summons allied Air Elementals for the duration of combat",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Summon_Air_Elemental.png`,
   },
   {
     id: "51",
@@ -138,6 +139,6 @@ export const airSpell: Array<Spell> = [
     level: 5,
     effect: "Inflicts 600 magic damage on a single enemy. " +
       "Requires Titan's Thunder",
-    imageUrl: "",
+    imageUrl: `${APP_DOMAIN}assets/images/spell/air/Titan's_Lightning_Bolt.png`,
   },
 ];
