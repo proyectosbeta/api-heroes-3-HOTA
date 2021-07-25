0bcdfd416fdd778b9234191686eef4d5d5aadce1
josego <josego85@gmail.com>
2021-07-25 12:14:15 -0400

    Add middleware notFound Page

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
-import { Application } from "./deps.ts";
+import { Application, yellow, green } from "./deps.ts";
 import { staticFileMiddleware } from "./app/middleware/staticFileMiddleware.ts";
 import router from "./app/routes/index.ts";
-import { APP_HOST, APP_PORT } from "./app/config/index.ts";
+import notFound from "./app/middleware/notFound.ts";
+import { APP_HOST, APP_PORT, APP_DOMAIN } from "./app/config/index.ts";
 
 const app = new Application();
 
@@ -9,5 +10,9 @@ app.use(staticFileMiddleware);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Listening on port ${APP_PORT} ...`);
+// 404 page.
+app.use(notFound);
+
+console.log(`${yellow("Listening on: ")} ${green(APP_DOMAIN)}`);
+
 await app.listen(`${APP_HOST}:${APP_PORT}`);
--- deps.ts
+++ deps.ts
@@ -1,3 +1,5 @@
 export { Application, Router, Context, send } from "https://deno.land/x/oak/mod.ts";
 
 export { config } from "https://deno.land/x/dotenv/mod.ts";
+
+export { green, yellow } from "https://deno.land/std/fmt/colors.ts";
