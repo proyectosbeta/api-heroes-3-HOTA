fa1693fcaa270b508f095c7aaeb8872449f7535b
josego <josego85@gmail.com>
2024-11-27 11:41:16 +0100

    refactor: use deno fmt

--- deps.ts
+++ deps.ts
@@ -1,5 +1,11 @@
-export { Application, Context, Response, Router, send } from 'https://deno.land/x/oak/mod.ts';
+export {
+  Application,
+  Context,
+  Response,
+  Router,
+  send,
+} from "https://deno.land/x/oak/mod.ts";
 
-export { config } from 'https://deno.land/x/dotenv/mod.ts';
+export { config } from "https://deno.land/x/dotenv/mod.ts";
 
-export { oakCors } from 'https://deno.land/x/cors/mod.ts';
+export { oakCors } from "https://deno.land/x/cors/mod.ts";
